5667b29196b2dad01835513a48854a3c91ce364b
jasbinder01 <hellojasbindersingh@gmail.com>
2025-06-11 19:11:33 +0530

    Update chipix.py

--- chipix.py
+++ chipix.py
@@ -12,8 +12,22 @@ import os
 # 1. FIREBASE SETUP
 # ────────────────────────────────────────────────────────────────────────────────
 if not firebase_admin._apps:
-    cred_path = os.path.join(os.path.dirname(__file__), "chipix.json")
-    cred = credentials.Certificate(cred_path)
+    firebase_config = st.secrets["firebase"]
+
+    cred = credentials.Certificate({
+        "type": firebase_config["type"],
+        "project_id": firebase_config["project_id"],
+        "private_key_id": firebase_config["private_key_id"],
+        "private_key": firebase_config["private_key"],
+        "client_email": firebase_config["client_email"],
+        "client_id": firebase_config["client_id"],
+        "auth_uri": firebase_config["auth_uri"],
+        "token_uri": firebase_config["token_uri"],
+        "auth_provider_x509_cert_url": firebase_config["auth_provider_x509_cert_url"],
+        "client_x509_cert_url": firebase_config["client_x509_cert_url"],
+        "universe_domain": firebase_config["universe_domain"]
+    })
+
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
